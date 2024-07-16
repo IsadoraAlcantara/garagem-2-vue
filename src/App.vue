406c088e7f7dfc3eb82ed73f77e9f790f9aa4c79
IsadoraAlcantara <Isadoraalca405@gmail.com>
2024-07-16 20:02:49 -0300

    criação do css da nav e da view acessórios

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,43 @@
 <script setup>
-import { onMounted } from 'vue';
-import { PassageUser } from '@passageidentity/passage-elements/passage-user';
-import { useAuthStore } from '@/stores/auth';
+import { onMounted } from 'vue'
+import { PassageUser } from '@passageidentity/passage-elements/passage-user'
+import { useAuthStore } from '@/stores/auth'
 
-const authStore = useAuthStore();
-
+const authStore = useAuthStore()
 </script>
 
 <template>
   <header>
-    <router-link :to="{ name: 'home' }">Home</router-link> |
-    <router-link to="/categorias">Categorias</router-link> |
-    <router-link to="/acessorios">Acessorios</router-link> |
-    <router-link to="/cores">Cores</router-link> |
-    <router-link to="/marcas">Marcas</router-link> |
-    <router-link to="/modelos">Modelos</router-link> |
-    <div v-if="authStore.loggedIn">
-    <router-link  to="/logout">Logout</router-link> |
-      {{ authStore.user.email }}
-    </div>
-    <router-link v-else to="/login">Login</router-link>
+    <nav>
+      <router-link :to="{ name: 'home' }">Home</router-link>
+      <router-link to="/categorias">Categorias</router-link>
+      <router-link to="/acessorios">Acessórios</router-link>
+      <router-link to="/cores">Cores</router-link>
+      <router-link to="/marcas">Marcas</router-link>
+      <router-link to="/modelos">Modelos</router-link>
+      <div v-if="authStore.loggedIn">
+        <router-link to="/logout">Logout</router-link>
+        {{ authStore.user.email }}
+      </div>
+      <router-link v-else to="/login">Login</router-link>
+    </nav>
   </header>
   <main>
     <RouterView />
   </main>
 </template>
+<style scoped>
+nav {
+  display: flex;
+  gap: 2vw;
+  justify-content: center;
+}
+
+nav a {
+  color: #797E8C;
+}
+
+nav a:hover {
+  color: #181B59;
+}
+</style>
--- src/views/AcessorioView.vue
+++ src/views/AcessorioView.vue
@@ -1,58 +1,103 @@
 <script setup>
-import { ref, reactive, onMounted } from "vue";
-import AcessoriosApi from "@/api/acessorios";
-const acessoriosApi = new AcessoriosApi();
+import { ref, reactive, onMounted } from 'vue'
+import AcessoriosApi from '@/api/acessorios'
+const acessoriosApi = new AcessoriosApi()
 
-const defaultAcessorio = { id: null, descricao: "" };
-const acessorios = ref([]);
-const acessorio = reactive({ ...defaultAcessorio });
+const defaultAcessorio = { id: null, descricao: '' }
+const acessorios = ref([])
+const acessorio = reactive({ ...defaultAcessorio })
 
 onMounted(async () => {
-  acessorios.value = await acessoriosApi.buscarTodosOsAcessorios();
-});
+  acessorios.value = await acessoriosApi.buscarTodosOsAcessorios()
+})
 
 function limpar() {
-  Object.assign(acessorio, { ...defaultAcessorio });
+  Object.assign(acessorio, { ...defaultAcessorio })
 }
 
 async function salvar() {
   if (acessorio.id) {
-    await acessoriosApi.atualizarAcessorio(acessorio);
+    await acessoriosApi.atualizarAcessorio(acessorio)
   } else {
-    await acessoriosApi.adicionarAcessorio(acessorio);
+    await acessoriosApi.adicionarAcessorio(acessorio)
   }
-  acessorios.value = await acessoriosApi.buscarTodosOsAcessorios();
-  limpar();
+  acessorios.value = await acessoriosApi.buscarTodosOsAcessorios()
+  limpar()
 }
 
 function editar(acessorio_para_editar) {
-  Object.assign(acessorio, acessorio_para_editar);
+  Object.assign(acessorio, acessorio_para_editar)
 }
 
 async function excluir(id) {
-  await acessoriosApi.excluirAcessorio(id);
-  acessorios.value = await acessoriosApi.buscarTodosOsAcessorios();
-  limpar();
+  await acessoriosApi.excluirAcessorio(id)
+  acessorios.value = await acessoriosApi.buscarTodosOsAcessorios()
+  limpar()
 }
 </script>
 
 <template>
-  <h1>Acessorio</h1>
-  <hr />
-  <div class="form">
-    <input type="text" v-model="acessorio.descricao" placeholder="Descrição" />
-    <button @click="salvar">Salvar</button>
-    <button @click="limpar">Limpar</button>
+  <h1>Acessório</h1>
+  <div class="container">
+    <div class="form">
+      <input type="text" v-model="acessorio.descricao" placeholder="Descrição" />
+      <button @click="salvar">Salvar</button>
+      <button @click="limpar">Limpar</button>
+    </div>
+    <ul>
+      <li v-for="acessorio in acessorios" :key="acessorio.id">
+        <span @click="editar(acessorio)"> ({{ acessorio.id }}) - {{ acessorio.descricao }} - </span>
+        <button @click="excluir(acessorio.id)">X</button>
+      </li>
+    </ul>
   </div>
-  <hr />
-  <ul>
-    <li v-for="acessorio in acessorios" :key="acessorio.id">
-      <span @click="editar(acessorio)">
-        ({{ acessorio.id }}) - {{ acessorio.descricao }} -
-      </span>
-      <button @click="excluir(acessorio.id)">X</button>
-    </li>
-  </ul>
 </template>
+<style scoped>
+h1 {
+  left: 45%;
+  color: #292d73;
+  font-weight: 600;
+  margin: 5vw 0 2vw 0;
+}
 
-<style></style>
+.container {
+  display: flex;
+  flex-direction: column;
+  padding: 1vw;
+  background-color: white;
+  width: 30vw;
+  min-height: 30vw;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  left: 35%;
+}
+
+.form {
+  display: flex;
+  gap: 0.6vw;
+  height: 3vw;
+}
+
+input {
+  padding: 0.8vw;
+  border-radius: 5px;
+  border: solid #797E8C 1px;
+  outline: none;
+  width: 30vw;
+}
+
+button {
+  background-color: #292d73;
+  color: white;
+  border: none;
+  padding: 0.6vw;
+  border-radius: 5px;
+}
+
+li {
+  background-color: white;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  list-style-type: none;
+  margin: 1vw;
+  border-radius: 4px;
+}
+</style>
